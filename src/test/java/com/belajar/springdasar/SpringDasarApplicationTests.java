264b23c7a22d4c072e715a81f38a451b1f6ec14a
Aclaputra <muhamadacla22@gmail.com>
2022-12-26 03:42:30 +0700

    feat: adding spring dasar application test

---
+++ src/test/java/com/belajar/springdasar/SpringDasarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belajar.springdasar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
