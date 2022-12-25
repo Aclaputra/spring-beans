b8518b4b69998acaded2ec1af3446271ad45ae13
Aclaputra <muhamadacla22@gmail.com>
2022-12-26 03:40:48 +0700

    feat: adding application context test

---
+++ src/test/java/com/belajar/springdasar/ApplicationContextTest.java
@@ -0,0 +1,16 @@
+package com.belajar.springdasar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextTest {
+
+  @Test
+  void testApplicationContext() {
+    ApplicationContext context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+
+    Assertions.assertNotNull(context);
+  }
+}
