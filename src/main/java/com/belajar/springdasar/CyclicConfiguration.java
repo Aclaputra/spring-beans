c56ce5108837d27a8d734e30cf45e08fb7fcaca2
Aclaputra <muhamadacla22@gmail.com>
2022-12-31 09:26:42 +0700

    feat: modified cyclic configuration

--- src/main/java/com/belajar/springdasar/CyclicConfiguration.java
+++ src/main/java/com/belajar/springdasar/CyclicConfiguration.java
@@ -1,2 +1,25 @@
-package com.belajar.springdasar;public class CyclicConfiguration {
+package com.belajar.springdasar;
+
+import com.belajar.springdasar.data.cyclic.CyclicA;
+import com.belajar.springdasar.data.cyclic.CyclicB;
+import com.belajar.springdasar.data.cyclic.CyclicC;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CyclicConfiguration {
+    @Bean
+    public CyclicA cyclicA(CyclicB cyclicB) {
+        return new CyclicA(cyclicB);
+    }
+
+    @Bean
+    public CyclicB cyclicB(CyclicC cyclicC) {
+        return new CyclicB(cyclicC);
+    }
+
+    @Bean
+    public CyclicC cyclicC(CyclicA cyclicA) {
+        return new CyclicC(cyclicA);
+    }
 }
