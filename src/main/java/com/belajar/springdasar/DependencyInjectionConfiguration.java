b678d28a0a8dbcc1cb54f2fb5edc1e4fe7ddcac0
Aclaputra <muhamadacla22@gmail.com>
2022-12-31 09:27:17 +0700

    feat: added dependency injection configuration

--- src/main/java/com/belajar/springdasar/DependencyInjectionConfiguration.java
+++ src/main/java/com/belajar/springdasar/DependencyInjectionConfiguration.java
@@ -1,2 +1,40 @@
-package com.belajar.springdasar;public class DependencyInjectionConfiguration {
+package com.belajar.springdasar;
+
+import com.belajar.springdasar.data.Bar;
+import com.belajar.springdasar.data.Foo;
+import com.belajar.springdasar.data.FooBar;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+public class DependencyInjectionConfiguration {
+    @Primary
+    @Bean
+    public Foo foo() {
+        return new Foo();
+    }
+
+    @Bean
+    public Foo fooSecond() {
+        return new Foo();
+    }
+
+    @Bean
+    public Bar bar() {
+        return new Bar();
+    }
+
+    /**
+     * if not using qualifier it will get the primary bean.
+     * use @qualifier to get specific bean.
+     * @param foo
+     * @param bar
+     * @return
+     */
+    @Bean
+    public FooBar fooBar(@Qualifier("fooSecond") Foo foo, Bar bar) {
+        return new FooBar(foo, bar);
+    }
+
 }
+
