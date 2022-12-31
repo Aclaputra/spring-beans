0062aeb73993992691379eb4bba35bc0b1981891
Aclaputra <muhamadacla22@gmail.com>
2022-12-31 09:28:29 +0700

    feat: added primary test

--- src/test/java/com/belajar/springdasar/PrimaryTest.java
+++ src/test/java/com/belajar/springdasar/PrimaryTest.java
@@ -1,2 +1,29 @@
-package com.belajar.springdasar;public class PrimaryTest {
+package com.belajar.springdasar;
+
+import com.belajar.springdasar.data.Foo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class PrimaryTest {
+
+    private ApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(PrimaryConfiguration.class);
+    }
+
+    @Test
+    void testGetPrimary() {
+        Foo foo = applicationContext.getBean(Foo.class);
+        Foo foo1 = applicationContext.getBean("foo1", Foo.class);
+        Foo foo2 = applicationContext.getBean("foo2", Foo.class);
+
+        Assertions.assertSame(foo, foo1);
+        Assertions.assertNotSame(foo, foo2);
+        Assertions.assertNotSame(foo1, foo2);
+    }
 }
