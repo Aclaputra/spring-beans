31fa48f2c2af07ea8a96a69cc5516b51a1fe05b4
Aclaputra <muhamadacla22@gmail.com>
2022-12-31 09:28:15 +0700

    feat: added primary configuration

--- src/main/java/com/belajar/springdasar/PrimaryConfiguration.java
+++ src/main/java/com/belajar/springdasar/PrimaryConfiguration.java
@@ -1,2 +1,22 @@
-package com.belajar.springdasar;public class PrimaryConfiguration {
+package com.belajar.springdasar;
+
+import com.belajar.springdasar.data.Foo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class PrimaryConfiguration {
+
+    @Primary
+    @Bean
+    public Foo foo1() {
+        return new Foo();
+    }
+
+    @Bean
+    public Foo foo2() {
+        return new Foo();
+    }
+
 }
