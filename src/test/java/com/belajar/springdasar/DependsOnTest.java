3d8c97aa70cb30c5d13516a75a5a8243a2ae9ee4
Aclaputra <muhamadacla22@gmail.com>
2023-01-01 11:03:26 +0700

    feat: added depends on test

--- src/test/java/com/belajar/springdasar/DependsOnTest.java
+++ src/test/java/com/belajar/springdasar/DependsOnTest.java
@@ -1,2 +1,22 @@
-package com.belajar.springdasar;public class DependsOnTest {
+package com.belajar.springdasar;
+
+import com.belajar.springdasar.data.Foo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class DependsOnTest {
+    private ApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(DependsOnConfiguration.class);
+    }
+    @Test
+    void testDependsOn() {
+        // it will not create bean at startup because its @lazy (only if called)
+        Foo foo = applicationContext.getBean(Foo.class);
+
+    }
 }
