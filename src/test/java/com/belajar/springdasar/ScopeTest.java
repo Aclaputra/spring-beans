247bf3e84193ca69ae1b29843585e52d51bc0adf
Aclaputra <muhamadacla22@gmail.com>
2023-01-01 11:04:30 +0700

    chore(beans): modified scope tes

--- src/test/java/com/belajar/springdasar/ScopeTest.java
+++ src/test/java/com/belajar/springdasar/ScopeTest.java
@@ -1,2 +1,30 @@
-package com.belajar.springdasar;public class ScopeTest {
+package com.belajar.springdasar;
+
+import com.belajar.springdasar.data.Foo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ScopeTest {
+    private ApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(ScopeConfiguration.class);
+    }
+
+    @Test
+    void testPrototypeScope() {
+        // it works if it's scope prototype not single ton.
+        // singleton will create bean only at startup while prototype will create only if called.
+        Foo foo1 = applicationContext.getBean(Foo.class);
+        Foo foo2 = applicationContext.getBean(Foo.class);
+        Foo foo3 = applicationContext.getBean(Foo.class);
+
+        Assertions.assertNotSame(foo1, foo2);
+        Assertions.assertNotSame(foo2, foo3);
+        Assertions.assertNotSame(foo3, foo1);
+    }
 }
