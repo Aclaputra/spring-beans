fa778b8d8b70d0603d01fc6dea134b3ea88de442
Aclaputra <muhamadacla22@gmail.com>
2023-01-01 11:05:03 +0700

    chore(beans): modified depends on configuration class

--- src/main/java/com/belajar/springdasar/DependsOnConfiguration.java
+++ src/main/java/com/belajar/springdasar/DependsOnConfiguration.java
@@ -5,15 +5,22 @@ import com.belajar.springdasar.data.Foo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Lazy;
 
 @Slf4j
 @Configuration
 public class DependsOnConfiguration {
+    @Lazy
     @Bean
+    @DependsOn({
+            "bar"
+    })
     public Foo foo() {
         log.info("Create new foo");
         return new Foo();
     }
+
     @Bean
     public Bar bar() {
         log.info("Create new bar");
